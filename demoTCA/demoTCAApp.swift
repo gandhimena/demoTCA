b17d9effa6b5a02fc153a4a5d080c7532e12d32f
gandhimenaumba <gandhimena@gmail.com>
2024-03-09 19:32:30 -0600

    Initial Commit

---
+++ demoTCA/demoTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoTCAApp.swift
+//  demoTCA
+//
+//  Created by gandhi mena on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct demoTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
